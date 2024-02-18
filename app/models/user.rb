2742036612967914a8c9a2693877b06328a9ec41
kurnik <c.jaworski@fu-berlin.de>
2024-02-18 02:02:24 +0100

    Initiall commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 12 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_many :posts, dependent: :destroy
+end
