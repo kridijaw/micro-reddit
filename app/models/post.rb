598ba69ff0ba32018f2ae0765f2d96b5074052cc
kurnik <c.jaworski@fu-berlin.de>
2024-02-19 01:02:07 +0100

    feat: Add validations and associations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
