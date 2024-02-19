67e29a55af57a2efcaa5be26329859073748d6ca
kurnik <c.jaworski@fu-berlin.de>
2024-02-19 01:02:25 +0100

    feat: Add comments table and foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_18_004852) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
+ActiveRecord::Schema[7.1].define(version: 2024_02_18_223735) do
+  create_table "comments", force: :cascade do |t|
     t.string "url"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -26,9 +36,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_18_004852) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
